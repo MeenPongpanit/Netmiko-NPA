730a70fcd68cc95dffe6ddd591d3521d73f52e60
NetAcad DEVASC <changeme@example.com>
2021-02-09 15:25:55 +0000

    Add device configuration script

--- netmiko01.py
+++ netmiko01.py
@@ -1,15 +1,30 @@
 from netmiko import ConnectHandler
 
-device_ip = '172.31.171.1'
 username = 'admin'
 password = 'cisco'
 
 device_params = {
     'device_type': 'cisco_ios',
-    'ip': device_ip,
+    # 'ip': device_ip,
     'username': username,
     'password': password,
 }
 
-with ConnectHandler(**device_params) as ssh:
-    ssh.send_config_from_file()
+devices_ip = {
+    'R0': '172.31.171.1',
+    'S0': '172.31.171.2',
+    'S1': '172.31.171.3',
+    'R1': '172.31.171.4',
+    'R2': '172.31.171.5',
+    'R3': '172.31.171.6',
+    'R4': '172.31.171.7',
+    'S2': '172.31.171.8',
+    'R5': '172.31.171.9',
+}
+
+for device in devices_ip:
+    device_params['ip'] = devices_ip[device]
+    with ConnectHandler(**device_params) as ssh:
+        print(f'Configuring {device}...')
+        ssh.send_config_from_file(f'./config/{device}.txt')
+        # print(ssh.send_command('sh ip int br'))
